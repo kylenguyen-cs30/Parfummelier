0805eee6b54abb94197ef55ae528eca738a04293
Kyle Nguyen <knguyen1193@outlook.com>
2024-09-10 11:59:37 -0700

    update error logs

--- services/user-service/app/models.py
+++ services/user-service/app/models.py
@@ -1,7 +1,9 @@
 from app import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False
+    password = db.Column(db.String(100), nullable=False)

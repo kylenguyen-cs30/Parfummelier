c57abc1730731635f442303123bda3bab21429ba
Kyle Nguyen <knguyen1193@outlook.com>
2024-09-09 23:51:27 -0700

    update user-service

--- services/user-service/app/__init__.py
+++ services/user-service/app/__init__.py
@@ -1,14 +1,26 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# from flask_cors import CORS
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://admin:password@db/users_db"
+
+    # CORS configurations
+
+    # app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://admin:password@db/users_db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+        "DATABASE_URL", "postgresql://admin:password@db/capstone_project"
+    )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
+    migrate.init_app(app, db)
 
     from app.routes import user_blueprint
 
--- services/user-service/app/models.py
+++ services/user-service/app/models.py
@@ -4,4 +4,4 @@ from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False

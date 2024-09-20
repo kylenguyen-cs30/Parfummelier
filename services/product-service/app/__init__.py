432c67e06e614942882d6d2cc1545e5e9692324f
Kyle Nguyen <knguyen1193@outlook.com>
2024-09-20 09:34:00 -0700

    temporary disable authentication-service

---
+++ services/product-service/app/__init__.py
@@ -0,0 +1,38 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+    CORS(
+        app,
+        resources={r"/*": {"origins": "http://localhost:3000"}},
+        methods=[
+            "GET",
+            "POST",
+            "DELETE",
+            "DELETE",
+            "OPTIONS",
+        ],
+        allow_headers=[
+            "Content-Type",
+            "Authorization",
+            "Access-Control-Allow-Credentials",
+        ],
+    )
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+        "DATABASE_URL", "postgresql://admin:password@db/capstone_project"
+    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+
+    from app.routes import product_blueprint
+
+    app.register_blueprint(product_blueprint)
+
+    return app

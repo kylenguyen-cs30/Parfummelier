8f61726fbc33cea60c9e2fbb92be1b5086f957e8
Kyle Nguyen <knguyen1193@outlook.com>
2024-10-02 00:28:13 -0700

    fix use-client

--- frontend/app/forget-password/page.tsx
+++ frontend/app/forget-password/page.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+"user server";
+import React, { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
 import Button from "../components/ui/button";

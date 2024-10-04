ef5757d001733cade5369832a55899d0a64bb679
khoa208 <khoadodev@gmail.com>
2024-10-03 22:24:11 -0700

    product-service/init.py, migrate

--- services/product-service/app/__init__.py
+++ services/product-service/app/__init__.py
@@ -2,9 +2,11 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)

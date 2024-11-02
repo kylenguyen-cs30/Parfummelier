e3bb4ef714ebf70ce9f203a76cf23a93ba36a345
Kyle Nguyen <knguyen1193@outlook.com>
2024-11-02 14:52:15 -0700

    update product-service

--- services/product-service/app/routes.py
+++ services/product-service/app/routes.py
@@ -84,7 +84,7 @@ def add_product():
         # Create a new product
         new_product = Product(
             name=data["name"],
-            designer=data.get("designer"),
+            # designer=data.get("designer"),
             # collection=collection,
         )
 
@@ -116,7 +116,7 @@ def add_product():
                 {
                     "id": new_product.id,
                     "name": new_product.name,
-                    "designer": new_product.designer,
+                    # "designer": new_product.designer,
                     "notes": [note.name for note in new_product.notes],
                     "accords": [accord.name for accord in new_product.accords],
                 },

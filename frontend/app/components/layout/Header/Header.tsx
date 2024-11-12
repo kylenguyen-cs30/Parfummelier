2afba614ca26cd90757b5151a438c230ecb81b2b
Kyle Nguyen <knguyen1193@outlook.com>
2024-11-12 12:25:55 -0800

    temp update for different computer

--- frontend/app/components/layout/Header/Header.tsx
+++ frontend/app/components/layout/Header/Header.tsx
@@ -7,6 +7,16 @@ import { useTheme } from "next-themes";
 import Link from "next/link";
 import Image from "next/image";
 
-const Header = () => {};
+const Header = () => {
+  const { user, logout } = useAuth();
+  const { theme, setTheme } = useTheme();
+
+  const navigation = {
+    products: [
+      { name: "All Products", href: "all-products" },
+      { name: "Product By Accord", href: "/product-by-accord" },
+    ],
+  };
+};
 
 export default Header;
